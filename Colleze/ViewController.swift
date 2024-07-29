93f53a23755e105ad24df69162ae03185908bf9b
Thrivikrama Sarma Bharathula <bhvs.thrivikram16@gmail.com>
2024-07-29 22:43:41 +0530

    feat: initial commit

---
+++ Colleze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colleze
+//
+//  Created by Apxor on 29/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
